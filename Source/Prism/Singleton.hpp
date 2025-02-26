380cf605f8fecd9bec2e85165eca54961e5b55f0
V1tr10l7 <vitriol1744@gmail.com>
2025-02-26 19:33:22 +0100

    [feat]: implement Singleton helper class

---
+++ Source/Prism/Singleton.hpp
@@ -0,0 +1,31 @@
+/*
+ * Created by v1tr10l7 on 26.02.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+namespace Prism
+{
+    template <typename T>
+    class Singleton
+    {
+      public:
+        // Gets instance of an object
+        static T* Instance()
+        {
+            static T instance;
+
+            return &instance;
+        }
+
+      protected:
+        Singleton()  = default;
+        ~Singleton() = default;
+
+      private:
+        Singleton(Singleton const&)            = default;
+        Singleton& operator=(Singleton const&) = default;
+    };
+}; // namespace Prism
