318e7aef4ab3b91a0f8987d85bbf5338bfe85820
V1tr10l7 <vitriol1744@gmail.com>
2025-03-21 01:34:45 +0100

    [feat]: add utils to convert epoch to date and time

---
+++ Source/Prism/Utility/Time.hpp
@@ -0,0 +1,89 @@
+/*
+ * Created by v1tr10l7 on 21.03.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Prism/Core/Types.hpp>
+
+namespace Prism::Time
+{
+    constexpr usize SECONDS_PER_MINUTE = 60;
+    constexpr usize SECONDS_PER_HOUR   = 3600;
+    constexpr usize SECONDS_PER_DAY    = 86400;
+
+    constexpr bool  IsLeapYear(u64 year)
+    {
+        return ((year % 4 == 0) && ((year % 100 != 0) || (year % 400) == 0));
+    }
+    constexpr u64 DaysInMonth(u8 month, u64 year)
+    {
+        constexpr u32 days[]
+            = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (month == 2 && IsLeapYear(year)) return 29;
+        return days[month];
+    }
+
+    struct DateTime
+    {
+        usize Year;
+        u8    Month;
+        u8    Day;
+        u8    Hour;
+        u8    Minute;
+        u8    Second;
+
+        constexpr DateTime() = default;
+        constexpr DateTime(u64 epoch)
+        {
+            u64 daysSinceEpoch   = epoch / SECONDS_PER_DAY;
+            u64 remainingSeconds = epoch % SECONDS_PER_DAY;
+
+            Hour                 = remainingSeconds / SECONDS_PER_HOUR;
+            remainingSeconds %= SECONDS_PER_HOUR;
+            Minute = remainingSeconds / SECONDS_PER_MINUTE;
+            Second = remainingSeconds % SECONDS_PER_MINUTE;
+
+            Year   = 1970;
+            for (;;)
+            {
+                u16 daysInYear = IsLeapYear(Year) ? 366 : 365;
+                if (daysSinceEpoch < daysInYear) break;
+                daysSinceEpoch -= daysInYear;
+                ++Year;
+            }
+
+            Month = 1;
+            while (daysSinceEpoch >= DaysInMonth(Month, Year))
+            {
+                daysSinceEpoch -= DaysInMonth(Month, Year);
+                ++Month;
+            }
+
+            Day = daysSinceEpoch + 1;
+        }
+    };
+}; // namespace Prism::Time
+
+template <>
+struct fmt::formatter<Prism::Time::DateTime> : fmt::formatter<std::string>
+{
+    template <typename FormatContext>
+    auto format(const Prism::Time::DateTime& date, FormatContext& ctx) const
+    {
+        return fmt::formatter<std::string>::format(
+            fmt::format("{:02}:{:02}:{:04} {:02}:{:02}:{:02}", date.Day,
+                        date.Month, date.Year, date.Hour, date.Minute,
+                        date.Second),
+            ctx);
+    }
+};
+
+#if PRISM_TARGET_CRYPTIX == 1
+using Prism::Time::DateTime;
+using Prism::Time::DaysInMonth;
+using Prism::Time::IsLeapYear;
+#endif
