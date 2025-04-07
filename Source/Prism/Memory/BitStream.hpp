3a2a07697c2f307587115ae2341822dde3c32077
V1tr10l7 <vitriol1744@gmail.com>
2025-04-07 02:32:37 +0200

    [feat]: add BitStream and DataStream classes

---
+++ Source/Prism/Memory/BitStream.hpp
@@ -0,0 +1,144 @@
+/*
+ * Created by v1tr10l7 on 04.04.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Prism/Core/Types.hpp>
+#include <Prism/Endian.hpp>
+#include <Prism/Memory/Pointer.hpp>
+
+namespace Prism
+{
+    // TODO(v1tr10l7): Handle BigEndian reads
+    template <Endian E = Endian::eNative>
+    class BitStream
+    {
+      public:
+        BitStream() = default;
+        BitStream(u8* data, usize size)
+            : m_CurrentPointer(data)
+            , m_StreamStart(data)
+            , m_StreamEnd(data + size)
+            , m_BitBuffer(0)
+            , m_BitCount(0)
+        {
+        }
+
+        inline bool IsEndOfStream() const
+        {
+            return m_CurrentPointer >= m_StreamEnd;
+        }
+
+        inline void Seek(usize offset)
+        {
+            m_CurrentPointer = m_StreamStart + offset;
+        }
+        inline usize Tell() const { return m_StreamEnd - m_CurrentPointer; }
+
+        template <std::integral T>
+        inline T Read()
+        {
+            assert(m_CurrentPointer + sizeof(T) <= m_StreamEnd);
+
+            T value;
+            std::memcpy(&value, m_CurrentPointer, sizeof(T));
+            m_CurrentPointer += sizeof(T);
+
+            return ConvertEndian<E>(value);
+        }
+        template <typename T>
+        inline usize Read(T* dest, usize count)
+        {
+            assert(m_CurrentPointer + count <= m_StreamEnd);
+            AlignToByteBoundary();
+
+            u8*   destBuffer = reinterpret_cast<u8*>(dest);
+            usize bytesRead  = 0;
+            if (m_BitCount > 0)
+            {
+                auto bitsToRead = std::min(count * 8, m_BitCount);
+                auto result     = ReadBits(bitsToRead);
+
+                bytesRead       = bitsToRead / 8;
+                std::memcpy(destBuffer, &result, bytesRead);
+                destBuffer += bytesRead;
+            }
+
+            std::memcpy(destBuffer, m_CurrentPointer, count - bytesRead);
+            m_CurrentPointer += count - bytesRead;
+
+            return count;
+        }
+
+        inline bool ReadBit() { return ReadBits(1); }
+        template <std::integral T = u64>
+        inline T ReadBits(usize count)
+            requires(sizeof(T) <= sizeof(u64))
+        {
+            auto result = PeekBits<T>(count);
+            DropBits(count);
+
+            return result;
+        }
+        template <std::integral T = u64>
+        inline T PeekBits(usize count)
+            requires(sizeof(T) <= 8)
+        {
+            if (count > m_BitCount) FillBits();
+
+            constexpr auto MAX_VALUE = std::numeric_limits<T>::max();
+            constexpr auto DIGITS    = std::numeric_limits<T>::digits;
+            count                    = std::min(count, m_BitCount);
+
+            return m_BitBuffer & (MAX_VALUE >> (DIGITS - count));
+        }
+
+        inline void DropBits(usize count)
+        {
+            if (count > m_BitCount) count = m_BitCount;
+
+            m_BitBuffer >>= count;
+            m_BitCount -= count;
+        }
+
+        inline u8 AlignToByteBoundary()
+        {
+            u8 remainingBits = 0;
+            if (auto offset = m_BitCount % 8; offset != 0)
+            {
+                remainingBits = m_BitBuffer & (Bit(offset) - 1);
+                DropBits(offset);
+            }
+
+            return remainingBits;
+        }
+
+        inline void FillBits()
+        {
+            do {
+                if (m_BitBuffer >= (1u << m_BitCount))
+                {
+                    m_CurrentPointer = m_StreamEnd;
+                    return;
+                }
+
+                m_BitBuffer |= static_cast<u32>(Read<u8>()) << m_BitCount;
+                m_BitCount += 8;
+            } while (m_BitCount <= 24);
+        }
+
+      private:
+        u8*   m_CurrentPointer = nullptr;
+        u8*   m_StreamStart    = nullptr;
+        u8*   m_StreamEnd      = nullptr;
+
+        u32   m_BitBuffer      = 0;
+        usize m_BitCount       = 0;
+    };
+}; // namespace Prism
+#if PRISM_TARGET_CRYPTIX == 1
+using Prism::BitStream;
+#endif
---
+++ Source/Prism/Memory/DataStream.hpp
@@ -0,0 +1,115 @@
+/*
+ * Created by v1tr10l7 on 03.04.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Prism/Core/Types.hpp>
+#include <Prism/Endian.hpp>
+
+#include <concepts>
+
+namespace Prism
+{
+    template <Endian E = Endian::eNative>
+    class DataStream
+    {
+      public:
+        DataStream() = default;
+        DataStream(u8* data, usize size)
+            : m_Data(data)
+            , m_Size(size)
+        {
+        }
+
+        constexpr u8*   begin() const { return m_Data; }
+        constexpr u8*   end() const { return m_Data + m_Offset; }
+
+        constexpr u8*   Raw() const { return m_Data; }
+        constexpr usize Size() const { return m_Size; }
+        constexpr usize Offset() const { return m_Offset; }
+        constexpr       operator bool() const { return m_Offset < m_Size; }
+
+        void            Load(u8* data, usize size)
+        {
+            m_Data = data;
+            m_Size = size;
+        }
+
+        inline constexpr void Seek(usize offset) { m_Offset = offset; }
+        inline constexpr void Skip(usize size)
+        {
+            if (m_Offset + size > m_Size) m_Offset = m_Size;
+            else m_Offset += size;
+        }
+
+        template <std::integral T>
+        T Read()
+        {
+            assert(m_Offset + sizeof(T) <= m_Size);
+
+            T value;
+            std::memcpy(&value, m_Data + m_Offset, sizeof(T));
+            m_Offset += sizeof(T);
+
+            return ConvertEndian<E>(value);
+        }
+        template <typename T>
+        void Read(T* dest, usize size)
+        {
+            assert(m_Offset + size <= m_Size);
+
+            std::memcpy(dest, m_Data + m_Offset, size);
+            m_Offset += size;
+        }
+
+        template <typename T>
+        void Write(T* data, usize size)
+        {
+            assert(m_Offset + size <= m_Size);
+
+            std::memcpy(m_Data + m_Offset, data, size);
+            m_Offset += size;
+        }
+        template <typename T, usize Size>
+        void operator<<(T (&dest)[Size])
+        {
+            assert(m_Offset + Size <= m_Size);
+
+            std::memcpy(reinterpret_cast<void*>(dest), m_Data + m_Offset, Size);
+            m_Offset += Size;
+        }
+
+        template <typename T>
+        void operator<<(T& dest)
+            requires(sizeof(T) > 8)
+        {
+            assert(m_Offset + sizeof(T) <= m_Size);
+
+            std::memcpy(&dest, m_Data + m_Offset, sizeof(T));
+            m_Offset += sizeof(T);
+        }
+        template <typename T>
+        void operator<<(T& dest)
+            requires(sizeof(T) <= 8)
+        {
+            assert(m_Offset + sizeof(T) <= m_Size);
+
+            T value;
+            std::memcpy(&value, m_Data + m_Offset, sizeof(T));
+
+            dest = ToEndian<E>(value);
+            m_Offset += sizeof(T);
+        }
+
+      private:
+        u8*   m_Data   = nullptr;
+        usize m_Size   = 0;
+        usize m_Offset = 0;
+    };
+}; // namespace Prism
+#if PRISM_TARGET_CRYPTIX == 1
+using Prism::DataStream;
+#endif
