3b8ec0d118663c3e8f40d2ed2e108ac7e9113c16
V1tr10l7 <vitriol1744@gmail.com>
2025-05-16 15:36:20 +0200

    [refactor]: check if builtin functions are available

--- Source/Prism/Core/Compiler.hpp
+++ Source/Prism/Core/Compiler.hpp
@@ -12,13 +12,41 @@
     #define PM_NOEXCEPT
 #endif
 
-#define PM_ALWAYS_INLINE           [[gnu::always_inline]]
-#define PM_USED [[gnu::used]]
-#define PM_PACKED [[gnu::packed]]
+#define PM_ALWAYS_INLINE [[gnu::always_inline]]
+#define PM_USED          [[gnu::used]]
+#define PM_PACKED        [[gnu::packed]]
 
-#define PM_LINE                 __builtin_LINE()
-#define PM_COLUMN               __builtin_COLUMN()
-#define PM_FILENAME             __builtin_FILE()
-#define PM_FUNCTION_NAME        __builtin_FUNCTION()
+#if defined(__has_builtin)
+    #if __has_builtin(__builtin_LINE)
+        #define PM_LINE __builtin_LINE()
+    #endif
+    #if __has_builtin(__builtin_COLUMN)
+        #define PM_COLUMN __builtin_COLUMN()
+    #endif
+    #if __has_builtin(__builtin_FILE)
+        #define PM_FILENAME __builtin_FILE()
+    #endif
+    #if __has_builtin(__builtin_FUNCTION)
+        #define PM_FUNCTION_NAME __builtin_FUNCTION()
+    #endif
+    #if __has_builtin(__builtin_frame_address)
+        #define PrismGetFrameAddress(n) __builtin_frame_address(n)
+    #endif
+#endif
 
-#define PrismGetFrameAddress(n) __builtin_frame_address(n)
+#ifndef PM_LINE
+    #define PM_LINE 0
+#endif
+
+#ifndef PM_COLUMN
+    #define PM_COLUMN 0
+#endif
+#ifndef PM_FILE
+    #define PM_FILE 0
+#endif
+#ifndef PM_FUNCTION_NAME
+    #define PM_FUNCTION_NAME 0
+#endif
+#ifndef PrismGetFrameAddress
+    #define PrismGetFrameAddress 0
+#endif
