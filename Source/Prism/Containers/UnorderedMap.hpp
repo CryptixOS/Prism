1664ef1a8f79440c23cb8e466db429fed822df3c
V1tr10l7 <vitriol1744@gmail.com>
2025-06-26 17:51:26 +0200

    [docs]: add documentation to the UnorderedMap

--- Source/Prism/Containers/UnorderedMap.hpp
+++ Source/Prism/Containers/UnorderedMap.hpp
@@ -19,9 +19,20 @@
 
 namespace Prism
 {
+    /**
+     * @brief Default hasher for keys used in UnorderedMap.
+     *
+     * @tparam K Type of the key.
+     */
     template <typename K>
     struct Hasher
     {
+        /**
+         * @brief Hash function for the key.
+         *
+         * @param key The key to hash.
+         * @return usize The resulting hash value.
+         */
         constexpr usize operator()(K key)
         {
             auto hash = std::hash<K>{}(key);
@@ -30,6 +41,13 @@ namespace Prism
         }
     };
 
+    /**
+     * @brief A custom hash map implementation using intrusive lists.
+     *
+     * @tparam K Key type.
+     * @tparam V Value type.
+     * @tparam H Hash function object type (defaults to Hasher<K>).
+     */
     template <typename K, typename V, typename H = Hasher<K>>
     class UnorderedMap
     {
@@ -38,17 +56,34 @@ namespace Prism
         using ValueType = V;
         using HashType  = H;
 
+        /**
+         * @brief Structure holding a key-value pair.
+         */
         struct KeyValuePair
         {
+            ///> The key
             KeyType   Key;
+            ///> The value
             ValueType Value;
 
+            /// @brief Default constructor.
             constexpr KeyValuePair() = default;
+            /**
+             * @brief Construct a key-value pair with lvalue reference.
+             * @param key The key.
+             * @param value The value.
+             */
             constexpr KeyValuePair(const KeyType& key, const ValueType& value)
                 : Key(key)
                 , Value(value)
             {
             }
+            /**
+             * @brief Construct a key-value pair with rvalue reference for
+             * value.
+             * @param key The key.
+             * @param value The value to move.
+             */
             constexpr KeyValuePair(const KeyType& key, ValueType&& value)
                 : Key(key)
                 , Value(Move(value))
@@ -56,25 +91,44 @@ namespace Prism
             }
         };
 
+        /**
+         * @brief Node stored in each bucket of the hash map.
+         */
         struct Node
         {
             using KeyType   = K;
             using ValueType = V;
 
+            ///> The key-value pair.
             KeyValuePair            Entry;
 
+            ///> Hook for intrusive list linking.
             IntrusiveListHook<Node> Hook;
             using List = IntrusiveList<Node>;
 
+            /**
+             * @brief Default constructor, enabled if key and value types are
+             * default-constructible.
+             */
             constexpr Node()
                 requires(IsDefaultConstructibleV<KeyType>
                          && IsDefaultConstructibleV<ValueType>)
             {
             }
+            /**
+             * @brief Construct a node with lvalue reference.
+             * @param key The key.
+             * @param value The value.
+             */
             constexpr Node(const KeyType& key, const ValueType& value)
                 : Entry(key, value)
             {
             }
+            /**
+             * @brief Construct a node with rvalue reference for value.
+             * @param key The key.
+             * @param value The value to move.
+             */
             constexpr Node(const KeyType& key, ValueType&& value)
                 : Entry(key, Move(value))
             {
@@ -84,6 +138,11 @@ namespace Prism
         using NodeType   = Node;
         using BucketType = IntrusiveList<Node>;
 
+        /**
+         * @brief Forward iterator for the UnorderedMap.
+         *
+         * @tparam Const Indicates if the iterator is const-qualified.
+         */
         template <bool Const = false>
         struct Iterator
         {
@@ -92,6 +151,10 @@ namespace Prism
             using EntryPointerType
                 = ConditionalType<Const, KeyValuePair* const, KeyValuePair*>;
 
+            /**
+             * @brief Constructs an iterator to the given bucket index in the
+             * map.
+             */
             constexpr Iterator(
                 ConditionalType<Const, const UnorderedMap*, UnorderedMap*> map,
                 const usize index)
@@ -104,6 +167,10 @@ namespace Prism
                 ListIt = m_Map->m_Buckets[m_BucketIndex].begin();
                 AdvanceToValid();
             }
+            /**
+             * @brief Constructs an iterator pointing to a specific element in a
+             * bucket.
+             */
             constexpr Iterator(
                 ConditionalType<Const, const UnorderedMap*, UnorderedMap*> map,
                 usize index, typename BucketType::Iterator listIt)
@@ -113,6 +180,9 @@ namespace Prism
             {
             }
 
+            /**
+             * @brief Advances to the next valid bucket.
+             */
             constexpr inline void AdvanceToValid()
             {
                 while (m_BucketIndex < m_Map->Capacity()
@@ -124,6 +194,7 @@ namespace Prism
                 }
             }
 
+            /// @brief Dereference operator.
             constexpr EntryRefType operator*()
             {
 #define PRISM_USE_INTRUSIVE_HASH_MAP 1
@@ -133,6 +204,7 @@ namespace Prism
                 return ListIt->Entry;
 #endif
             }
+            /// @brief Arrow operator.
             constexpr EntryPointerType operator->()
             {
 #if PRISM_USE_INTRUSIVE_HASH_MAP == 0
@@ -143,6 +215,7 @@ namespace Prism
 #endif
             }
 
+            /// @brief Prefix increment operator.
             constexpr Iterator& operator++()
             {
                 ++ListIt;
@@ -157,6 +230,7 @@ namespace Prism
                 return *this;
             }
 
+            /// @brief Equality operator.
             constexpr bool operator==(const Iterator& other) const
             {
                 return (m_BucketIndex >= m_Map->Capacity()
@@ -164,6 +238,7 @@ namespace Prism
                     || (m_BucketIndex == other.m_BucketIndex
                         && ListIt == other.ListIt);
             }
+            /// @brief Inequality operator.
             constexpr bool operator!=(const Iterator& other) const
             {
                 return !(*this == other);
@@ -179,28 +254,61 @@ namespace Prism
         using ReverseIterator      = ::Prism::ReverseIterator<Iterator<false>>;
         using ConstReverseIterator = ::Prism::ReverseIterator<Iterator<true>>;
 
+        /**
+         * @brief Constructs the unordered map with optional initial capacity.
+         *
+         * @param initialCapacity Number of buckets to preallocate.
+         */
         constexpr UnorderedMap(usize initialCapacity = 8);
+        /// @brief Destructor.
         constexpr ~UnorderedMap();
 
+        /// @brief Checks if the map is empty.
         constexpr bool  Empty() const PM_NOEXCEPT { return m_Size == 0; }
+        /// @brief Returns the number of key-value pairs in the map.
         constexpr usize Size() const PM_NOEXCEPT { return m_Size; }
+        /// @brief Returns the number of buckets allocated.
         constexpr usize Capacity() const PM_NOEXCEPT
         {
             return m_Buckets.Size();
         }
 
+        /**
+         * @brief Returns the value associated with a key.
+         * @param key The key to search for.
+         * @return Value reference.
+         * @throws If key does not exist.
+         */
         constexpr ValueType&       At(const KeyType& key);
+        /// @copydoc At(const KeyType&)
         constexpr const ValueType& At(const KeyType& key) const;
+        /**
+         * @brief Returns a reference to value associated with key, inserting if
+         * not found.
+         */
         constexpr ValueType&       operator[](const KeyType& key);
+        /// @copydoc operator[](const KeyType&)
         constexpr ValueType&       operator[](KeyType&& key);
 
+        /**
+         * @brief Finds an element by key.
+         *
+         * @param key The key to search for.
+         * @return Iterator to found element or end().
+         */
         constexpr Iterator<>       Find(const K& key);
+        /// @copydoc Find(const K&)
         constexpr ConstIterator    Find(const K& key) const;
+        /**
+         * @brief Checks if the key exists in the map.
+         */
         constexpr bool             Contains(const K& key) const
         {
             return Find(key) != end();
         }
 
+        /// @name Iterators
+        /// @{
         constexpr Iterator<>           begin();
         constexpr ConstIterator        begin() const;
         constexpr ConstIterator        cbegin() const PM_NOEXCEPT;
@@ -218,24 +326,60 @@ namespace Prism
         constexpr ReverseIterator      rend();
         constexpr ConstReverseIterator rend() const;
         constexpr ConstReverseIterator crend() const PM_NOEXCEPT;
+        /// @}
 
+        /// @brief Removes all elements from the map.
         constexpr void                 Clear() PM_NOEXCEPT;
+        /**
+         * @brief Rehashes the map to at least a new number of buckets.
+         */
         constexpr void                 Rehash(usize count);
+        /**
+         * @brief Reserves capacity for at least a certain number of elements.
+         */
         constexpr void                 Reserve(usize capacity);
+        /**
+         * @brief Ensures current load factor is under threshold, rehashing if
+         * necessary.
+         */
         constexpr void                 EnsureCapacity();
 
+        /**
+         * @brief Inserts a new key-value pair.
+         */
         constexpr Iterator<>           Insert(const K& key, const V& value);
+        /// @copydoc Insert(const K&, const V&)
         constexpr Iterator<>           Insert(const K& key, V&& value);
+        /**
+         * @brief Inserts a pre-allocated node into the map.
+         */
         constexpr Iterator<>           Insert(Node* node);
+        /**
+         * @brief Constructs a key-value pair in-place.
+         *
+         * @tparam Args Argument types for key/value.
+         * @param args Arguments to forward to constructor.
+         */
         template <typename... Args>
         constexpr Iterator<> Emplace(Args&&... args);
+        /**
+         * @brief Removes a key-value pair by key.
+         *
+         * @param key Key to remove.
+         * @return Iterator pointing to next valid element.
+         */
         constexpr Iterator<> Erase(const K& key);
 
       private:
+        ///> Buckets holding linked lists of nodes.
         Vector<BucketType> m_Buckets;
+        ///> Number of elements in the map.
         usize              m_Size                  = 0;
+        ///> Load factor numerator.
         usize              m_LoadFactorNumerator   = 3;
+        ///> Load factor denominator.
         usize              m_LoadFactorDenominator = 4;
+        ///> Maximum load factor threshold.
         usize              m_MaxLoadFactor         = 1;
     };
 } // namespace Prism
