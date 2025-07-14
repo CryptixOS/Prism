25bbf2e185e601d2824b5d9b83567630422d3838
V1tr10l7 <vitriol1744@gmail.com>
2025-07-14 17:31:51 +0200

    [feat]: implement printf argument parsing

---
+++ Source/Prism/String/Printf.cpp
@@ -0,0 +1,154 @@
+/*
+ * Created by v1tr10l7 on 24.03.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#include <Prism/String/Printf.hpp>
+#include <Prism/String/StringUtils.hpp>
+
+namespace Prism::Printf
+{
+    FormatSpec FormatParser::operator()(StringView::Iterator& it, va_list& args)
+    {
+        FormatSpec specs;
+
+        struct
+        {
+            enum State     State = State::eBegin;
+            constexpr void operator()(enum State state)
+            {
+                if (State >= state) assert("invalid format specifier");
+                State = state;
+            }
+        } enterState;
+
+        for (; *it;)
+        {
+            switch (*it)
+            {
+                case '\0': return specs;
+                case '%': 
+                    enterState(State::eBegin); break;
+                    ++it;
+                    if (*it == '%')
+                    {
+                        specs.Type = ArgumentType::eChar;
+                        return specs;
+                    }
+                    continue;
+                case '-':
+                    enterState(State::eFlags);
+                    specs.JustifyLeft = true;
+                    break;
+                case '+':
+                    enterState(State::eFlags);
+                    specs.Sign = Sign::ePlus;
+                    break;
+                case ' ':
+                    enterState(State::eFlags);
+                    specs.Sign = Sign::eSpace;
+                    break;
+                case '#':
+                    enterState(State::eFlags);
+                    specs.PrintBase = true;
+                    break;
+                case '0':
+                    enterState(State::eFlags);
+                    specs.ZeroPad = true;
+                    break;
+                case '1' ... '9':
+                {
+                    enterState(State::eWidth);
+                    const char* start = it;
+                    for (; StringUtils::IsDigit(*it); it++);
+
+                    StringView len(start, it - start);
+                    specs.Length = ToNumber<isize>(len, 10);
+
+                    continue;
+                }
+                case '*':
+                    enterState(State::eWidth);
+                    specs.Length = static_cast<isize>(va_arg(args, i32));
+                    break;
+                case '.':
+                {
+                    enterState(State::ePrecision);
+                    ++it;
+                    const char* start = it;
+                    for (; StringUtils::IsDigit(*it); it++);
+
+                    usize      strLength = it - start;
+                    StringView len(start, strLength);
+                    if (!len.Empty())
+                    {
+                        specs.Precision = ToNumber<isize>(len, 10);
+                        continue;
+                    }
+                    else if (*it == '*')
+                        specs.Precision = static_cast<isize>(va_arg(args, i32));
+                    else
+                        assert(
+                            "Printf: Invalid format specifier, parsing "
+                            "precision failed");
+                }
+                case 'l':
+                    enterState(State::eLength);
+                    specs.Type = ArgumentType::eLong;
+                    ++it;
+                    if (*it == 'l') specs.Type = ArgumentType::eLongLong;
+                    else continue;
+                    break;
+                case 'z':
+                    enterState(State::eLength);
+                    specs.Type = ArgumentType::eSize;
+                    break;
+
+                default: goto loop_end;
+            }
+            it++;
+        }
+    loop_end:
+
+        switch (*it)
+        {
+            case 'b':
+                specs.Base = 2;
+                specs.Type
+                    = static_cast<ArgumentType>(ToUnderlying(specs.Type) + 5);
+                break;
+            case 'o':
+                specs.Base = 8;
+                specs.Type
+                    = static_cast<ArgumentType>(ToUnderlying(specs.Type) + 5);
+                break;
+            case 'p':
+                specs.Type = ArgumentType::eSize;
+                specs.Type
+                    = static_cast<ArgumentType>(ToUnderlying(specs.Type) + 5);
+                specs.Base = 16;
+                break;
+            case 'X': specs.UpperCase = true;
+            case 'x':
+                specs.Base = 16;
+                if (specs.Type < ArgumentType::eInteger)
+                    specs.Type = ArgumentType::eInteger;
+                specs.Type
+                    = static_cast<ArgumentType>(ToUnderlying(specs.Type) + 5);
+                break;
+            case 'u':
+                specs.Type
+                    = static_cast<ArgumentType>(ToUnderlying(specs.Type) + 5);
+                break;
+            case 'd':
+            case 'i': break;
+            case 'c': specs.Type = ArgumentType::eChar; break;
+            case 's': specs.Type = ArgumentType::eString; break;
+            case 'n': specs.Type = ArgumentType::eOutWrittenCharCount; break;
+        }
+        ++it;
+
+        return specs;
+    }
+}; // namespace Prism::Printf
---
+++ Source/Prism/String/Printf.hpp
@@ -0,0 +1,81 @@
+/*
+ * Created by v1tr10l7 on 24.03.2025.
+ * Copyright (c) 2024-2025, Szymon Zemke <v1tr10l7@proton.me>
+ *
+ * SPDX-License-Identifier: GPL-3
+ */
+#pragma once
+
+#include <Prism/Core/Types.hpp>
+#include <Prism/String/StringView.hpp>
+
+namespace Prism
+{
+    namespace Printf
+    {
+        enum class ArgumentType
+        {
+            eChar                = 0,
+            eInteger             = 1,
+            eLong                = 2,
+            eLongLong            = 3,
+            eSize                = 4,
+
+            eUnsignedChar        = 5,
+            eUnsignedInteger     = 6,
+            eUnsignedLong        = 7,
+            eUnsignedLongLong    = 8,
+            eUnsignedSize        = 9,
+            eLastArithmetic      = 10,
+            eString              = 11,
+            eOutWrittenCharCount = 12,
+        };
+
+        enum class Sign
+        {
+            eNone  = 0,
+            eMinus = 1,
+            ePlus  = 2,
+            eSpace = 3,
+        };
+        struct FormatSpec
+        {
+            ArgumentType   Type            = ArgumentType::eInteger;
+            u8             Base            = 10;
+            isize          Length          = -1;
+            isize          Precision       = 0;
+            enum Sign      Sign            = Sign::eNone;
+
+            bool           JustifyLeft : 1 = false;
+            bool           PrintBase   : 1 = false;
+            bool           ZeroPad     : 1 = false;
+            bool           UpperCase   : 1 = false;
+
+            constexpr bool Arithmetic() const
+            {
+                return Type < ArgumentType::eLastArithmetic;
+            }
+        };
+        struct FormatParser
+        {
+            enum class State
+            {
+                eBegin     = 0,
+                eFlags     = 1,
+                eWidth     = 2,
+                ePrecision = 3,
+                eLength    = 4,
+                eSpecifier = 5,
+            };
+
+            FormatSpec operator()(StringView::Iterator& it, va_list& args);
+        };
+
+        isize Formatted(const char* format, ...);
+        isize Variadic(const char* format, va_list& args);
+    }; // namespace Printf
+}; // namespace Prism
+
+#if PRISM_TARGET_CRYPTIX != 0
+namespace Printf = Prism::Printf;
+#endif
